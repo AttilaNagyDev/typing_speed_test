abf128476d1bd11a54917b96014ec1175dc2cc0f
Attila Nagy <nagyattila.mail@gmail.com>
2025-08-15 21:06:09 +0100

    started with TKinter boilerplate

---
+++ main.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+GREY = "#5C6672"
+FONT = ("Arial", 14)
+
+
+# ------------------------------------------------------ UI SETUP ------------------------------------------------------
+app = Tk()
+app.title("Typing Speed Test")
+app.config(padx=25, pady=35, bg=GREY, width=800, height=400)
+app.resizable(width=False, height=False)
+
+
+app.mainloop()
